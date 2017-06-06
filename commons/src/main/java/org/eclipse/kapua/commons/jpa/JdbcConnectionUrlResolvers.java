79b0123e7f3ed308f11788f242f61330fdd78ab1
huangguohua <huangguohua@izhiju.cn>
2017-06-06 10:33:08 +0800

    add mysql/mariadb url support

--- commons/src/main/java/org/eclipse/kapua/commons/jpa/JdbcConnectionUrlResolvers.java
+++ commons/src/main/java/org/eclipse/kapua/commons/jpa/JdbcConnectionUrlResolvers.java
@@ -33,7 +33,7 @@ public final class JdbcConnectionUrlResolvers {
         } else if (connectionUrlResolverType.equals("H2")) {
             return new H2JdbcConnectionUrlResolver().connectionUrl();
         } else if (connectionUrlResolverType.equals("mysql")||connectionUrlResolverType.equals("mariadb")) {
-            return new H2JdbcConnectionUrlResolver().connectionUrl();
+            return new MariaDBJdbcConnectionUrlResolver().connectionUrl();
         } else {
             throw new IllegalArgumentException("Unknown JDBC connection URL resolver type: " + connectionUrlResolverType);
         }
